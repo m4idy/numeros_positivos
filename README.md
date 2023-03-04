ff1696bce4d3cad1632832b29115342649a1908f
m4idy <maidy2234507@correo.uis.edu.co>
2023-03-03 23:05:51 -0500

    numeros_positivos

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # numeros_positivos
 Implementar un programa en Python para verificar si un numero es positivo y de cuatro cifras
+
+# Analisis
+
+# Diseño
+
+### Diagrama de flujo
+![diagrama de flujo](diagrama.png "diagrama de flujo")
---
+++ numeros_positivos.py
@@ -0,0 +1,34 @@
+
+
+print(".................................................................")
+print("..............Numero positivo y de cuatro cifras.................")
+print(".................................................................")
+
+# imput 
+n= int(input("digite un numero"))
+
+
+# auptut
+
+if(n>0 ):
+        
+    print("el numero es positivo")
+
+else:
+
+    (n<0)
+    
+    print("el numero es negativo")
+
+if(10000>n>999):
+
+    rta="es un numero de cuatro cifras"
+
+else:
+
+    rta="no es de cuatro digitos"
+
+
+print("el valor " +str(rta))
+
+
